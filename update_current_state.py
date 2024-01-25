a5965fbfcb7f31a58e2e1b312b35d13ca221a6e5
Thomarini <62741938+Thomarini@users.noreply.github.com>
2024-01-25 23:58:48 +0100

    Update update_current_state.py

--- update_current_state.py
+++ update_current_state.py
@@ -9,8 +9,13 @@ def updateState(state, dt):
     ds = (state[3]*dt + state[4]*dt)/2
     dtheta = (state[3]*dt - state[4]*dt)/e    
 
+    # Soit - sur le sinus soit - sur théta (state[2][0]).
+    # à cause de l'orientation négative de théta dans le repère ?
+        
     state[0] = state[0] + ds*np.cos(state[2] + dtheta/2)
-    state[1] = state[1] - ds*np.sin(state[2] + dtheta/2) # jsp pourquoi il faut un -
+    state[1] = state[1] - ds*np.sin(state[2] + dtheta/2) 
+    # Modulo utilisé pour l'affichage dans la console 
+    # à voir si l'utilisation en embarqué profiterait de son absence (mémoire vs temps de calcul)
     state[2]  = (state[2] + dtheta + np.pi)%(2*np.pi)-np.pi
         
     #TODO : update l'état des capteurs
